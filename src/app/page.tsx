5f41384bc6ed70554f6888777766f0e6a025e013
levi <guncow4336@gmail.com>
2024-09-06 11:10:27 +0900

    component upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
     const [bottomSheeetOpen, setBottomSheetOpen] = useState(false);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <button onClick={() => {setBottomSheetOpen(true)}}>
+        open
+      </button>
       <BottomSheet onClose={() => {setBottomSheetOpen(false)}} open={bottomSheeetOpen} >
         <div className="flex flex-col gap-6  pt-6">
           <div className="text-xxl font-semiBold">
